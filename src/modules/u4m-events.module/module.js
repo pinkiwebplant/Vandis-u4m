4967930404697ade47cc23b9c2cba80df7e5c2f0
pinkiwebplant <pinki@thewebplant.in>
2025-03-19 10:53:35 +0530

    css update

--- src/modules/u4m-events.module/module.js
+++ src/modules/u4m-events.module/module.js
@@ -1,6 +1,23 @@
-$('.u4m-events .pagination a').on('click', function(e){
-	e.preventDefault();
-  var loadUrl = $(this).attr('data-href')
-  console.log(loadUrl); 
-  $( ".postCardsWrap" ).load(loadUrl + " .postCardsInr");
+document.addEventListener("click", function (e) {
+	var target = e.target.closest(".u4m-events .pagination a");
+
+	if (target) {
+		e.preventDefault();
+		var loadUrl = target.getAttribute("data-href");
+		console.log("Loading:", loadUrl);
+
+		fetch(loadUrl)
+			.then(response => response.text())
+			.then(html => {
+			let parser = new DOMParser();
+			let doc = parser.parseFromString(html, "text/html");
+			let newContent = doc.querySelector(".postCardsInr");
+
+			if (newContent) {
+				document.querySelector(".postCardsWrap").innerHTML = newContent.innerHTML;
+				console.log("Content loaded successfully");
+			}
+		})
+			.catch(error => console.error("Error loading content:", error));
+	}
 });
